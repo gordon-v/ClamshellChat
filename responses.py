e5fd013b3adad98ddabafab0db95187f00661719
gordon-v <gordon.velichkovski@proton.me>
2023-04-20 18:00:36 +0200

    Add project files.

---
+++ responses.py
@@ -0,0 +1,14 @@
+﻿def get_response(message: str) -> str:
+    p_message = message.lower()
+
+    if 'hello' in p_message:
+        return 'hai :3'
+
+    if 'hi' in p_message:
+       return 'haiiiii'
+
+    if 'hai' in p_message:
+        return 'haiiiiiiiii:3333:33:333:3333'
+    if 'nig' in p_message:
+       return '📸'
+
